74407a5af94d535c688da377aac42e0f121fea63
Louis Lee <louis.lee2@griffithuni.edu.au>
2018-09-03 08:05:33 +1000

    test2

---
+++ angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent} from './login/login.component';
+
+const routes: Routes = [
+  {path:'home', component:LoginComponent},
+  {path:'index', component:LoginComponent},
+  {path:'chat', component:ChatComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
